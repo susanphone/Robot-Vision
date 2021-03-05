000fe43095c42de4d90226e6f88c0ba207d315a2
Susan McCartney <susan.a.mccartney12@gmail.com>
2021-03-05 09:49:53 -0700

    hw3

--- Homework3/FollowTheFruit.py
+++ Homework3/FollowTheFruit.py
@@ -1,8 +1,21 @@
+# Follow the Fruit Assignment
+# Maria Gallivan and Susan McCartney
+# March 5, 2021
+
 from controller import *
 import cv2 as cv
 import numpy as np
 
-# use object tracking from last program
+robot = Robot()
+print("hello")
+
+leftMotor = robot.getDevice('left wheel motor')
+rightMotor = robot.getDevice('right wheel motor')
+leftMotor.setPosition(float('inf'))
+rightMotor.setPosition(float('inf'))
+leftMotor.setVelocity(0.0)
+rightMotor.setVelocity(0.0)
+
 global hh
 global hv
 global hs
@@ -18,10 +31,10 @@ def getValues(event, x, y, flags, param):
         colorsS = hsv[y, x, 1]
         colorsV = hsv[y, x, 2]
         colors = hsv[y, x]
-        print("Hue: ", colorsH)
-        print("Saturation: ", colorsS)
-        print("Lightness: ", colorsV)
-        print("BRG Format: ", colors)
+        print("Hue: \t", colorsH)
+        print("Saturation: \t", colorsS)
+        print("Lightness: \t", colorsV)
+        print("BRG Format: \t", colors)
         print("Coordinates of pixel: X: ", x, "Y: ", y)
     return
 
@@ -29,38 +42,74 @@ def getValues(event, x, y, flags, param):
 def nothing(x):
     pass
 
-# TODO: capture video from e-puck
-cap = cv.VideoCapture(0)
+
+# capture video from e-puck
+timestep = int(robot.getBasicTimeStep())
+
+cam = robot.getDevice("camera")
+cam.enable(timestep)
+
 cv.namedWindow("Video")
+cv.setMouseCallback("Video", getValues)
 
+while robot.step(timestep) != -1:
+    img = cam.getImage()
+    width = cam.getWidth()
+    height = cam.getHeight()
+    temppic = np.frombuffer(img, np.uint8)
+    out = np.reshape(temppic, (height, width, 4))
+    cv.imshow("Video", out)
 
-while(True):
+    #Creates a threshold for the orange colors
+    hsv = cv.cvtColor(out, cv.COLOR_BGR2HSV)
+    min = np.array([19, 132, 138])
+    max = np.array([21, 150, 155])
 
-    status, src = cap.read()
+    mask = cv.inRange(hsv, min, max)
 
-    hsv = cv.cvtColor(src, cv.COLOR_BGR2HSV)
+    res = cv.bitwise_and(out, out, mask=mask)
 
-    cv.imshow("Video", hsv)
+    #calculates number of pixels in the threshold
+    n_white_pix = np.sum(res == 255)
+    print('Number of white pixels:', n_white_pix)
 
-    cv.setMouseCallback("Video", getValues)
-# TODO: keep e-puck a set distance away from orange
+    #grab contours
+    gray_image = cv.cvtColor(res, cv.COLOR_BGR2GRAY)
+    ret, thresh = cv.threshold(gray_image, 127, 255, 0)
+    M = cv.moments(thresh)
 
-min = np.array([lh, ls, lv])
-max = np.array([hh, hs, hv])
+    # calculate x,y coordinate of center
+    if M["m00"] != 0:  # checks for division by zero
+        x = int(M["m10"] / M["m00"])
+        y = int(M["m01"] / M["m00"])
+        print('Center (', x, ', ', y, ')')
+    else:
+        x = 140
+        y = 0
+        print('Center (NA, NA)')
 
-mask = cv.inRange(hsv, min, max)
+    #Rotates the robot left and right
+    if x > 170:
+        print('Turn right')
+        leftMotor.setVelocity(2.5)
+        rightMotor.setVelocity(0.0)
+    elif x < 90:
+        print('Turn left')
+        leftMotor.setVelocity(0.0)
+        rightMotor.setVelocity(2.5)
 
-res = cv.bitwise_and(src, src, mask=mask)
+    #Moves the robot forward and backwards
+    if n_white_pix > 920:
+        print('Move backwards')
+        leftMotor.setVelocity(-5.0)
+        rightMotor.setVelocity(-5.0)
+    elif n_white_pix < 720:
+        print('Move forwards')
+        leftMotor.setVelocity(5.0)
+        rightMotor.setVelocity(5.0)
 
-mask_3 = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)
-stacked = np.hstack((mask_3, src, res))
-cv.imshow("Original", src)
-cv.imshow("Mask", mask)
-cv.imshow("Res", res)
+    k = cv.waitKey(1)
+    if k == 27:
+        break
 
-if cv.waitKey(1) & 0xFF == ord("q"):
-    cv.destroyAllWindows()
-    break
-
-cap.release()
 cv.destroyAllWindows()
